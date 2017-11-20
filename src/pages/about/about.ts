2c4edf60a795baf72efff54b1b47ba6b5eb6d3bc
Gowthaman Ramasamy <ragowthaman@gmail.com>
2017-11-20 23:52:50 +0530

    improve bidding display

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,15 +1,39 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 import { HttpServerServiceProvider } from '../../providers/http-server-service/http-server-service';
+
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  domestic_quotes: any[] = [];
+  todate: any = new Date().toISOString().split('T')[0];
+  bidding_history: any[];
+  latest_bid_info: any = {};
+  order_quantity: any;
+    
   bidding_list: any[] = [];
-  constructor(public navCtrl: NavController, private httpServerServiceProvider: HttpServerServiceProvider) {
-    console.log('about constructor');
+  constructor(public navCtrl: NavController, private httpServerServiceProvider: HttpServerServiceProvider, private storage: Storage, private toastCtrl: ToastController) {
+    this.httpServerServiceProvider.getAllDomesticQuotesWithLatestBid().subscribe((data) => {
+      console.log(data);
+      this.domestic_quotes = data;
+    });
   }
+
+  
+  doRefresh(event) {
+    this.httpServerServiceProvider.getAllDomesticQuotesWithLatestBid().subscribe((data) => {
+      console.log(data);
+      this.domestic_quotes = data;
+      event.complete();
+    }, (error) => {
+      event.complete();
+    });
+  }
+
+  
 
   getBiddingList(event) {
     this.httpServerServiceProvider.getDomesticBiddingListByBuyer().subscribe((data) => {
