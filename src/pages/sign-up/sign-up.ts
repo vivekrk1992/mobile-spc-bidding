3ef3e3419919de98f4651cfed6c7e19fdad081b5
Guru <vajralaguru@gmail.com>
2018-03-06 12:49:45 +0530

    Pop up for OTP

--- src/pages/sign-up/sign-up.ts
+++ src/pages/sign-up/sign-up.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { HttpServerServiceProvider } from '../../providers/http-server-service/http-server-service';
-
+import { AlertController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -14,7 +14,7 @@ export class SignUpPage {
   sign_up_form: FormGroup;
   show_otp_field: boolean = false;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder, private httpServerServiceProvider: HttpServerServiceProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder, private httpServerServiceProvider: HttpServerServiceProvider, private alertController: AlertController) {
     this.sign_up_form = this.formBuilder.group({
       username: [null],
       first_name: [null],
@@ -34,10 +34,46 @@ export class SignUpPage {
     console.log(signup_value);
     this.httpServerServiceProvider.signUp(signup_value).subscribe((data) => {
       console.log(data);
-      this.show_otp_field = true;
+      // this.show_otp_field = true;
+      this.presentConfirm();
     }, (error) => {
       console.log(error);
     });
+  }
+
+
+  presentConfirm() {
+    let alert = this.alertController.create({
+      title: 'Enter OTP',
+      inputs: [
+        {
+          name: 'otp',
+          placeholder: 'Enter OTP',
+          type: 'number'
+        },
+        
+      ],
+      // message: 'Do you want to buy this book?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Confirm',
+          handler: (data) => {
+            console.log('Confirm clicked');
+            data['Mobile'] = this.sign_up_form.value['city'];
+            console.log(data);
+
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 
 }
--- src/providers/global/global.ts
+++ src/providers/global/global.ts
@@ -3,7 +3,8 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class GlobalProvider {
   private _bag_quantity = 50;
-  private _base_url: string = 'http://192.168.0.109:8000/';
+  private _base_url: string = 'http://127.0.0.1:8000/';
+  // private _base_url: string = 'http://192.168.0.107:8000/';
 
   get bag_quantity() {
     return this._bag_quantity
