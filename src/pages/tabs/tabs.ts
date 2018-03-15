a3821dc0d0fc037372de8fd06315a5cdedc2c91c
Guru <vajralaguru@gmail.com>
2018-03-15 11:07:40 +0530

    sale list for three days displayed on Griveance page

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,69 +1,28 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
 import { GrievancePage } from './../grievance/grievance';
-import { BuyerProfilePage } from './../buyer-profile/buyer-profile';
 import { OrderPage } from './../order/order';
 import { Storage } from '@ionic/storage';
-import { NavController, Tabs } from 'ionic-angular';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  @ViewChild('tabRoot') tabRef: Tabs;
-  // tab1Root = HomePage;
   tab1Root = OrderPage;
-  // tab1Root = BuyerProfilePage;
   tab2Root = AboutPage;
   tab3Root = ContactPage;
   tab4Root = GrievancePage;
-  buyer_profile_complete_percentage: number = 0;
   user: any;
 
-  constructor(private storage: Storage, public navCtrl: NavController) {
-
-    // this.tab1Root = OrderPage;
+  constructor(private storage: Storage) {
     storage.get('user').then((user) => {
       console.log(user);
       this.user = user;
     }, (error) => {
       console.log(error);
     });
-
-    this.storage.get('user_properties').then((property) => {
-      if (property.hasOwnProperty('buyer_profile_complete_percentage')) {
-        this.buyer_profile_complete_percentage = parseInt(property['buyer_profile_complete_percentage']);
-        console.log('profile complete percentage = ', this.buyer_profile_complete_percentage);
-        console.log(typeof (this.buyer_profile_complete_percentage));
-        this.routePage();
-      } else {
-        this.buyer_profile_complete_percentage = 0;
-      }
-    }, (error) => {
-      console.log(error);
-    });
-    
-  }
-  
-  // getActiveChildNav() {
-  //   this.routePage();
-  // }
-  // ionViewCanEnter() {
-  //   this.routePage();
-  // }
-
-  routePage() {
-    if (this.buyer_profile_complete_percentage == 100) {
-      console.log('in if condition when profile is at 100');
-      // this.navCtrl.setRoot(OrderPage);
-      // this.tab1Root = OrderPage;
-      // this.tabRef.select(0)
-    } else {
-      console.log('in else for second if');
-      this.navCtrl.setRoot(BuyerProfilePage);
-    }
   }
 }
