2763353388a807014313444b5e58fced2652cedd
Guru <vajralaguru@gmail.com>
2018-10-11 16:05:06 +0530

    buyer approval system enabled

--- src/pages/order-tabs/order-tabs.ts
+++ src/pages/order-tabs/order-tabs.ts
@@ -4,15 +4,15 @@ import { ContactPage } from '../contact/contact';
 // import { HomePage } from '../home/home';
 import { GrievancePage } from './../grievance/grievance';
 // import { OrderPage } from './../order/order';
-// import { MultiOrderPage } from './../multi-order/multi-order';
+import { MultiOrderPage } from './../multi-order/multi-order';
 
 @Component({
   templateUrl: 'order-tabs.html'
 })
 export class OrderTabsPage {
 
-  tab1Root = 'ChatPage';
-  // tab1Root = MultiOrderPage;
+  // tab1Root = 'ChatPage';
+  tab1Root = MultiOrderPage;
   tab2Root = AboutPage;
   tab3Root = ContactPage;
   tab4Root = GrievancePage;
--- src/providers/global/global.ts
+++ src/providers/global/global.ts
@@ -3,10 +3,10 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class GlobalProvider {
   private _bag_quantity = 50;
-  private _app_version: string = '0.1.8';
-  // private _base_url: string = 'http://api.exportcopra.com/';
-  // private _base_url: string = 'http://192.168.0.4:8000/';
-  private _base_url: string = 'http://127.0.0.1:8000/';
+  private _app_version: string = '0.1.10';
+  private _base_url: string = 'http://api.exportcopra.com/';
+  // private _base_url: string = 'http://192.168.0.5:8000/';
+  // private _base_url: string = 'http://127.0.0.1:8000/';
 
   get bag_quantity() {
     return this._bag_quantity
